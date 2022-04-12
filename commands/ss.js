e48a7aff5d01920ddbac5ed287d577e815e21b11
Redeamer <wesleytuuh@gmail.com>
2022-04-12 02:07:18 +0200

    finished for now
    needs refactor for expandability

--- commands/ss.js
+++ commands/ss.js
@@ -1,20 +1,37 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
-const { joinVoiceChannel, getVoiceConnection } = require('@discordjs/voice');
-const wait = require('node:timers/promises').setTimeout;
+const { joinVoiceChannel, createAudioPlayer, createAudioResource, AudioPlayerStatus } = require('@discordjs/voice');
+const wait = require('node:timers/promises').setTimeout();
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('ss')
         .setDescription('Missing ping!'),
+        
     async execute(interaction) {
         const voiceChannel = interaction.member.voice.channel;
+
         if (!voiceChannel) await interaction.reply("Join a voice channel first!")
+
         const voiceChannelConnection = joinVoiceChannel({
             channelId: voiceChannel.id,
             guildId: voiceChannel.guild.id,
             adapterCreator: voiceChannel.guild.voiceAdapterCreator
         });
-        await wait(2000);
-        voiceChannelConnection.destroy();
+        const pingAudio = createAudioResource("../ping_missing.mp3")
+        const player = createAudioPlayer();
+
+        voiceChannelConnection.subscribe(player);
+
+        player.play(pingAudio);
+
+        interaction.reply("MISSING!");
+
+        player.on(AudioPlayerStatus.Idle, () => {
+            player.stop();
+            voiceChannelConnection.destroy();
+
+        })
+
+        interaction.deleteReply();
     },
 };
