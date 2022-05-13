1a24a8428d91d2ce9f6cf9d817ccec38f6e18d04
Redeamer <wesleytuuh@gmail.com>
2022-05-13 22:35:06 +0200

    Format file

--- deploy-commands.js
+++ deploy-commands.js
@@ -9,7 +9,7 @@ dotenv.config();
 const commands = [];
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
-for (const file of commandFiles){
+for (const file of commandFiles) {
     const command = require(`./commands/${file}`);
     commands.push(command.data.toJSON());
 }
@@ -18,5 +18,5 @@ const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
 
 // Now pushes global commands instead of guild specific. Global commands take an hour or so to cache and take effect on servers this bot is on.
 rest.put(Routes.applicationCommands(process.env.CLIENT_ID), { body: commands })
-	.then(() => console.log('Successfully registered application commands.'))
-	.catch(console.error);
+    .then(() => console.log('Successfully registered application commands.'))
+    .catch(console.error);
